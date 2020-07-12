73c129c80960d5147821d4d8589b4a5427efab60
Lostr9 <dima.gasii@icloud.com>
2020-07-12 21:20:45 +0400

    Изменение модели

--- src/main/java/ru/lostr/watchingprice/model/WatchingPage.java
+++ src/main/java/ru/lostr/watchingprice/model/WatchingPage.java
@@ -1,6 +1,13 @@
 package ru.lostr.watchingprice.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
 public class WatchingPage implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer watchingPageId;
 }
