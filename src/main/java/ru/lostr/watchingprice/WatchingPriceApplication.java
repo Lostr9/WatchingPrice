335021e5321217273959a8fbbe12f2d4d72e6e06
Dmitrii <35877491+Lostr9@users.noreply.github.com>
2020-07-12 21:04:38 +0400

    Инициализация проекта

---
+++ src/main/java/ru/lostr/watchingprice/WatchingPriceApplication.java
@@ -0,0 +1,13 @@
+package ru.lostr.watchingprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchingPriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchingPriceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/lostr/watchingprice/WatchingPriceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lostr.watchingprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchingPriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
